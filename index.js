1a28083cc934cf052a7503c5cf90540b9a847f4c
mistaken-shot <gye161203@gyeseong.es.kr>
2025-09-14 18:16:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,45 +5,46 @@ let number = document.getElementById("number")
 let now_card_id = 0 
 const card  = []
 class Card{
-    constructor(card_id,name,answer){
+    constructor(card_id,name,answer,blind){
             this.card_id = card_id;
             this.name = name;
             this.answer = answer;
+            this.blind = blind;
     }
 }
 const initialCards = [
-    { name: "1차 세계대전 발발", answer: "1914" },
-    { name: "1차 세계대전 종전", answer: "1918" },
-    { name: "범죄 즉결례", answer: "1912-03-18" },
-    { name: "조선 태형령", answer: "1912-03-18" },
-    { name: "경찰범 처벌 규칙", answer: "1912-03-25" },
-    { name: "3.1운동", answer: "1919-03-01" },
-    { name: "쌀 소동", answer: "1918" },
-    { name: "치안 유지법", answer: "1925-04-27" },
-    { name: "만주 사변", answer: "1931-09-18" },
-    { name: "대공황", answer: "1929" },
-    { name: "아시아 태평양 전쟁", answer: "1941" },
-    { name: "2차 세계대전 발발", answer: "1939" },
-    { name: "2차 세계대전 종전", answer: "1945" },
-    { name: "치안 유지법", answer: "1925-04-27" },
-    { name: "진주만 기습", answer: "1942-06" },
-    { name: "과달카날 전투 발발", answer: "1942-08" },
-    { name: "과달카날 전투 종전", answer: "1943-02" },
-    { name: "히로시마 원폭 투하", answer: "1945-08-06" },
-    { name: "나가사키 원폭 투하", answer: "1945-08-09" },
-    { name: "소련 참전", answer: "1945-08" },
-    { name: "국가 총동원법", answer: "1938" },
-    { name: "『국민 총력』 발행 시작", answer: "1939" },
-    { name: "지원병제 실시", answer: "1938" },
-    { name: "학도 지원병 제도 실시", answer: "1943" },
-    { name: "징병제 실시", answer: "1944" }
+    { name: "1차 세계대전 발발", answer: "1914" ,blind:"????"},
+    { name: "1차 세계대전 종전", answer: "1918" ,blind:"????"},
+    { name: "범죄 즉결례", answer: "1912-03-18",blind:"????-??-??" },
+    { name: "조선 태형령", answer: "1912-03-18",blind:"????-??-??" },
+    { name: "경찰범 처벌 규칙", answer: "1912-03-25",blind:"????-??-??" },
+    { name: "3.1운동", answer: "1919-03-01",blind:"????-??-??" },
+    { name: "쌀 소동", answer: "1918" ,blind:"????"},
+    { name: "치안 유지법", answer: "1925-04-27",blind:"????-??-??" },
+    { name: "만주 사변", answer: "1931-09-18",blind:"????-??-??" },
+    { name: "대공황", answer: "1929" ,blind:"????"},
+    { name: "아시아 태평양 전쟁", answer: "1941",blind:"????" },
+    { name: "2차 세계대전 발발", answer: "1939",blind:"????" },
+    { name: "2차 세계대전 종전", answer: "1945",blind:"????" },
+    { name: "치안 유지법", answer: "1925-04-27",blind:"????-??-??" },
+    { name: "진주만 기습", answer: "1942-06",blind:"????-??" },
+    { name: "과달카날 전투 발발", answer: "1942-08",blind:"????-??" },
+    { name: "과달카날 전투 종전", answer: "1943-02",blind:"????-??" },
+    { name: "히로시마 원폭 투하", answer: "1945-08-06",blind:"????-??-??" },
+    { name: "나가사키 원폭 투하", answer: "1945-08-09",blind:"????-??-??" },
+    { name: "소련 참전", answer: "1945-08" ,blind:"????-??"},
+    { name: "국가 총동원법", answer: "1938",blind:"????" },
+    { name: "『국민 총력』 발행 시작", answer: "1939",blind:"????" },
+    { name: "지원병제 실시", answer: "1938",blind:"????" },
+    { name: "학도 지원병 제도 실시", answer: "1943",blind:"????" },
+    { name: "징병제 실시", answer: "1944",blind:"????" }
     
     
 ];
 
 // 초기 카드 삽입
 initialCards.forEach(item => {
-    card.push(new Card(random_id(), item.name, item.answer));
+    card.push(new Card(random_id(), item.name, item.answer,item.blind));
 });
 function random_id(){
     now_card_id = Math.floor(Math.random() * (99999));
@@ -76,7 +77,7 @@ function show_card(){
     console.log(system_type);
  if (c && system_type === "1") {
         document.getElementById("output").innerHTML = 
-            `ID: ${c.card_id}<br>Question: ${c.name}<br>Answer: ????-??-??`;
+            `ID: ${c.card_id}<br>Question: ${c.name}<br>Answer: ${c.blind}`;
     } else if(c && system_type==="0"){
         document.getElementById("output").innerHTML = 
             `ID: ${c.card_id}<br>Question: ${c.name}<br>Answer: ${c.answer}`;
@@ -107,7 +108,7 @@ function show_random_card(){
 
     if (c && system_type === "1") {
         document.getElementById("output").innerHTML = 
-            `ID: ${c.card_id}<br>Question: ${c.name}<br>Answer: ????/??/??`;
+            `ID: ${c.card_id}<br>Question: ${c.name}<br>Answer: ${c.blind}`;
     } else if (c && system_type === "0") {
         document.getElementById("output").innerHTML = 
             `ID: ${c.card_id}<br>Question: ${c.name}<br>Answer: ${c.answer}`;
